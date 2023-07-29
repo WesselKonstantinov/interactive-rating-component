5c7c166c4bc15de5a6cbd10aa2d122e1df4403f0
Wessel Konstantinov <wes_kon1@hotmail.com>
2023-07-29 16:40:37 +0200

    Complete submission logic

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const radioButtons = document.querySelectorAll(".js-radio");
+const submitButton = document.querySelector(".js-submit");
+
+radioButtons.forEach((radioButton) => {
+  radioButton.addEventListener("change", () => {
+    submitButton.removeAttribute("disabled");
+  });
+});
+
 const form = document.querySelector(".js-form");
 const rating = document.querySelector(".js-rating");
 const thankYouSection = document.querySelector(".js-thanks");
@@ -5,7 +14,7 @@ const thankYouSection = document.querySelector(".js-thanks");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const selectedRating = e.target.rating.value;
-  e.target.parentElement.hidden = "true";
+  e.target.parentElement.classList.add("card--hidden");
   rating.innerText = selectedRating;
   thankYouSection.classList.remove("card--hidden");
 });
